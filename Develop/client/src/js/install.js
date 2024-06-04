d1d7e31d77e7ed455b17ccaa8e5a476cfbdf418b
Justin Ramos <Jramos20022@gmail.com>
2024-06-03 20:01:25 -0400

    base code

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,11 +1,39 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the mini-infobar from appearing on mobile
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  // Update UI notify the user they can install the PWA
+  butInstall.style.display = "block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    // The deferred prompt isn't available.
+    return;
+  }
+  // Show the install prompt
+  promptEvent.prompt();
+  // Wait for the user to respond to the prompt
+  const result = await promptEvent.userChoice;
+  console.log("User choice", result);
+  // We've used the prompt, and can't use it again, throw it away
+  window.deferredPrompt = null;
+  // Hide the install button
+  butInstall.style.display = "none";
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("PWA was installed");
+  // Clear the deferredPrompt so it can be garbage collected
+  window.deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  // e.g., logInstall(event);
+});
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,29 +1,70 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      // Generates the index.html file
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        title: "Text Editor",
+      }),
+      // Injects the custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+      // Generates the manifest.json file
+      new WebpackPwaManifest({
+        name: "Text Editor",
+        short_name: "Editor",
+        description: "A text editor application",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        start_url: "./index.html",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/icon-192x192.png"),
+            sizes: [192, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // Babel loader for ES6+ syntax
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
